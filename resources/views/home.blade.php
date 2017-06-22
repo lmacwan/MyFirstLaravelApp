a92b186f84404accf5087ed6418fd2bb42df2601
Leon Macwan <lmacwan@hotmail.com>
2017-06-22 09:54:53 +0530

    Implemented Pages - About and Contact

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,11 @@
-<html>
-<head>
-    <title>Home Page</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+@extends('master')
+@section('title', 'Home')
 
-	<script src="//code.jquery.com/jquery-3.1.1.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="container">
-    <div class="content">
-        <div class="title">Home Page</div>
-        <div class="quote">My First Laravel App Home page!</div>
+@section('content')
+    <div class="container">
+        <div class="content">
+            <div class="title">Home Page</div>
+            <div class="quote">My First Laravel App Home page!</div>
+        </div>
     </div>
-</div>
-</body>
-</html>
+@endsection
